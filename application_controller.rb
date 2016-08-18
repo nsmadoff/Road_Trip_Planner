c03b8b99bccc64c1972ad67aa65aec2fa82c8833
daliad123 <darwish.dalia@gmail.com>
2016-08-18 14:36:06 +0000

    Project day2 part 1

--- application_controller.rb
+++ application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/destination' do
+    erb :index
+  end
+
+  post '/destination' do
+    erb :dest.erb
+  end
 end
